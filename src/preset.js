9357cde708ecc83b682571e7623c95a3b8a0b1ae
Lynh <jenky.w0w@gmail.com>
2020-12-29 16:14:39 +0700

    Upgrade to apply

--- src/preset.js
+++ src/preset.js
@@ -1,85 +1,87 @@
-const { Preset } = require('use-preset');
+import { Preset } from 'apply'
 
-module.exports = Preset.make('Igniter')
-  .option('interaction', true)
-  .option('schema', false)
-  .prompts()
-    .confirm('Do you want to set default string length to `191` for schema migration', 'schema')
-    .chain()
-  .copyTemplates('ask')
-  .edit('app/Providers/AppServiceProvider.php')
-    .if(({ flags, prompts }) => Boolean(flags.schema) || Boolean(prompts.schema))
-    .title('Set Schema default length')
-    .search(/use Illuminate\\Support\\ServiceProvider;/)
-      .addBefore('use Illuminate\\Support\\Facades\\Schema;')
-      .end()
-    .search(/public function boot\(\)/)
-      .addAfter([
-        `{`,
-        `    Schema::defaultStringLength(191);`,
-      ])
-      .removeAfter(2) // Removes opening curly bracket and comment
-      .end()
-    .chain()
-  .editJson('composer.json')
-    .title('Setup Composer dependencies')
-    .merge({
-      require: {
-        'doctrine/dbal': '^2.10'
-      },
-      'require-dev': {
-        'friendsofphp/php-cs-fixer': '^2.16'
-      },
-      autoload: {
-        files: [
-          'app/Support/helpers.php'
-        ]
+Preset.setName('Igniter')
+
+Preset.extract()
+
+Preset.confirm('schema', 'Do you want to set default string length to `191` for schema migration', true)
+
+Preset.group(preset => {
+  preset.edit('app/Providers/AppServiceProvider.php')
+    .addAfter('ServiceProvider;', 'use Illuminate\\Support\\Facades\\Schema;')
+
+  preset.edit('app/Providers/AppServiceProvider.php')
+    .addAfter('public function boot', 'Schema::defaultStringLength(191);')
+    .skipLines(1)
+    .withIndent('double')
+})
+  .ifPrompt('schema')
+  .withTitle('Set Schema default length')
+
+Preset.editPhpPackages()
+  .withTitle('Setup Composer dependencies')
+  .add('doctrine/dbal', '^2.10')
+  .addDev('friendsofphp/php-cs-fixer', '^2.16')
+  .set('autoload.files', ['app/Support/helpers.php'])
+
+// Preset.group(preset => {
+//   preset.editNodePackages()
+//     .remove('axios')
+//     .remove('lodash')
+//     .add('axios', '^0.19')
+//     .add('lodash', '^4.17.13')
+//     .addDev('husky', '^4.2.5')
+//     .addDev('lint-staged', '^10.2.5')
+
+//   preset.editNodePackages().merge({
+//     husky: {
+//       hooks: {
+//         'pre-commit': 'lint-staged'
+//       }
+//     },
+//     'lint-staged': {
+//       '*.php': 'php ./vendor/bin/php-cs-fixer fix --config .php_cs'
+//     }
+//   })
+// })
+
+Preset.editJson('package.json')
+  .delete([
+    'devDependencies.axios',
+    'devDependencies.lodash',
+  ])
+  .withTitle('Setup Node dependencies')
+  .merge({
+    dependencies: {
+      axios: '^0.19',
+      lodash: '^4.17.13'
+    },
+    devDependencies: {
+      husky: '^4.2.5',
+      'lint-staged': '^10.2.5'
+    },
+    husky: {
+      hooks: {
+        'pre-commit': 'lint-staged'
       }
-    })
-    .chain()
-  .editJson('package.json')
-    .delete([
-      'devDependencies.axios',
-      'devDependencies.lodash',
-    ])
-    .title('Setup Node dependencies')
-    .merge({
-      dependencies: {
-        axios: '^0.19',
-        lodash: '^4.17.13'
-      },
-      devDependencies: {
-        husky: '^4.2.5',
-        'lint-staged': '^10.2.5'
-      },
-      husky: {
-        hooks: {
-          'pre-commit': 'lint-staged'
-        }
-      },
-      'lint-staged': {
-        '*.php': 'php ./vendor/bin/php-cs-fixer fix --config .php_cs'
-      }
-    })
-    .chain()
-  .edit('.gitignore')
-    .title('Update .gitignore')
-    .search(/\.phpunit\.result\.cache/)
-      .addAfter('.php_cs.cache')
-      .end()
-    .chain()
-  .edit('config/logging.php')
-    .title('Set stack log channels to daily')
-    .replace(`['single']`)
-      .with(`['daily']`)
-    .chain()
-  .installDependencies()
-    .if(({ flags }) => Boolean(flags.interaction))
-    .for('node')
-    .title('Install Node dependencies')
-    .chain()
-  .updateDependencies()
-    .if(({ flags }) => Boolean(flags.interaction))
-    .for('php')
-    .title('Install PHP dependencies')
-    .chain();
+    },
+    'lint-staged': {
+      '*.php': 'php ./vendor/bin/php-cs-fixer fix --config .php_cs'
+    }
+  })
+
+Preset.edit('.gitignore')
+  .withTitle('Update .gitignore')
+  .addAfter('.phpunit.result.cache', '.php_cs.cache')
+
+Preset.edit('config/logging.php')
+  .update(content => content.replace(`['single']`, `['daily']`))
+  .withTitle('Set stack log channels to daily')
+
+Preset.installDependencies('node')
+  .ifUserApproves()
+  .withTitle('Install Node dependencies')
+
+Preset.installDependencies('php')
+  .ifUserApproves()
+  .withTitle('Install PHP dependencies')
