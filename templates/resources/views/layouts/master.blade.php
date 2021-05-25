4fdda04e09320983459949c4f40fefe297390a23
Lynh <jenky.w0w@gmail.com>
2021-05-25 17:43:04 +0700

    Update layout

--- templates/resources/views/layouts/master.blade.php
+++ templates/resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     @section('head')
     <meta charset="@yield('charset', 'utf-8')">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @stack('meta')
@@ -11,11 +11,6 @@
     <title>@yield('title', config('app.name'))</title>
 
     @stack('css')
-
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
     @show
 </head>
