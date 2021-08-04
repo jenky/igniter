d48ea2b5da5e0a0e81e1a050f1a634770b6979ce
Lynh <jenky.w0w@gmail.com>
2021-08-04 16:05:19 +0700

    Remove meta csrf

--- templates/resources/views/layouts/base.blade.php
+++ templates/resources/views/layouts/base.blade.php
@@ -4,7 +4,6 @@
     @section('head')
     <meta charset="@yield('charset', 'utf-8')">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @stack('meta')
 
